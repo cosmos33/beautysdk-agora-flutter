143ef49d26c7d5fd943c4b389a584b7269a77967
fanqiang <fan.qiang_2499@immomo.com>
2021-10-13 17:07:49 +0800

    init

---
+++ example/lib/config/agora.config.dart
@@ -0,0 +1,14 @@
+/// Get your own App ID at https://dashboard.agora.io/
+const appId = "3d49d27e09094021bb2a3ec60ac555ed";
+
+/// Please refer to https://docs.agora.io/en/Agora%20Platform/token
+const token = "0063d49d27e09094021bb2a3ec60ac555edIACwu2rr7UU++FiEYAbPriIfXYx0eaXGLSPBkKTWvyGwiMBT1aoAAAAAEACXXodmtoBmYQEAAQC1gGZh";
+
+/// Your channel ID
+const channelId = "mmbeautytest";
+
+/// Your int user ID
+const uid = 111;
+
+/// Your string user ID
+const stringUid = "";
